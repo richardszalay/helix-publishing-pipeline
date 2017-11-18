6c0835f5aa37a84b60228ec22aac123eb9785546
Richard Szalay <richard@richardszalay.com>
2017-11-18 17:48:56 +1100

    Create separate WebRoot / Module configurations
    WebRoot 'owns' the Web.config

---
+++ src/targets/tests/WebRoot.Acceptance.Tests.ps1
@@ -0,0 +1,86 @@
+. "$PSScriptRoot/utils/WebConfig.ps1"
+. "$PSScriptRoot/utils/MSBuild.ps1"
+. "$PSScriptRoot/utils/MSDeploy.ps1"
+
+$fixtures = @{
+    default = @{
+        Solution = "$PSScriptRoot\fixtures/default/HelixBuild.Sample.Web.sln";
+        Project1 = "$PSScriptRoot\fixtures/default/Projects\HelixBuild.Sample.Web\HelixBuild.Sample.Web.csproj";
+    }
+    
+}
+
+$count = 1
+
+Describe "WebRoot configuration" {
+
+    Context "building package with default settings" {
+        $projectPath = $fixtures.default.Project1
+        $projectDir = Split-Path $projectPath -Parent
+        
+        Invoke-MSBuild -Project $projectPath -Properties @{
+            "HelixTargetsConfiguration" = "WebRoot";
+            "Configuration" = "Debug";
+            "DeployOnBuild" = "true";
+            "PublishProfile" = "Package";
+            "DeployAsIisApp" = "false";
+        }
+
+        $packageFilename = Join-Path $projectDir "obj\Debug\Package\HelixBuild.Sample.Web.zip"
+
+        $packageParameters = Get-MSDeployPackageParameters $packageFilename
+        $packageParameterNames = $packageParameters.Keys
+
+        $packageFiles = Get-MSDeployPackageFiles $packageFilename
+
+        $webConfigXml = [xml](Get-MSDeployPackageFileContent -PackagePath $packageFilename -FilePath "Web.config")
+
+        It "should include web deploy parameters from the packaged project" {
+            $packageParameterNames -contains "Project-Parameter1" | Should Be $true
+        }
+
+        It "should include web deploy parameters from direct module dependencies" {
+            $packageParameterNames -contains "Feature1-Parameter1" | Should Be $true
+        }
+
+        It "should not include web deploy parameters from indirect module dependencies" {
+            $packageParameterNames -contains "Foundation1-Parameter1" | Should Be $false
+        }
+
+        It "should include content from the packaged project" {
+            $packageFiles -contains "App_Config\Include\HelixBuild.Sample.Web.config" | Should Be $true
+        }
+
+        It "should include content from direct module dependencies" {
+            $packageFiles -contains "App_Config\Include\HelixBuild.Feature1.config" | Should Be $true  
+        }
+
+        It "should not include content from indirect module dependencies" {
+            $packageFiles -contains "App_Config\Include\HelixBuild.Foundation1.config" | Should Be $false
+        }
+
+        It "should include Web.config from the packaged project" {
+            (Get-WebConfigAppSetting $webConfigXml "HelixProject") | Should Be "Sample.Web"
+        }
+
+        It "should not include include Web.config from direct module dependencies" {
+            (Get-WebConfigAppSetting $webConfigXml "HelixBuild.Feature1") | Should Be $null
+        }
+
+        It "should not include include Web.config from indirect module dependencies" {
+            (Get-WebConfigAppSetting $webConfigXml "HelixBuild.Foundation1") | Should Be $null
+        }
+
+        It "should include standard Web.config transforms from the packaged project" {
+            (Get-WebConfigAppSetting $webConfigXml "Project.ConfigKey") | Should Be "Project.ConfigValue"
+        }
+
+        It "should include Web.Helix.config transforms from feature modules" {
+            (Get-WebConfigAppSetting $webConfigXml "Feature1.ConfigKey") | Should Be "Feature1.ConfigValue"
+        }
+
+        It "should not include Web.Helix.config transforms from indirect module dependencies" {
+            (Get-WebConfigAppSetting $webConfigXml "Foundation1.ConfigKey") | Should Be $null
+        }
+    }
+}
