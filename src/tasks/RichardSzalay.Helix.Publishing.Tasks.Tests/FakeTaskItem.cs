9b5505fe7c595f26c43c26e350b88b5a1c17b60b
Richard Szalay <richard@richardszalay.com>
2018-03-22 22:04:01 +1100

    Skip transformed config files if unchanged
    Prevents an app pool recycle

---
+++ src/tasks/RichardSzalay.Helix.Publishing.Tasks.Tests/FakeTaskItem.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Microsoft.Build.Framework;
+
+namespace RichardSzalay.Helix.Publishing.Tasks.Tests
+{
+    public class FakeTaskItem : ITaskItem, IEnumerable<KeyValuePair<string, string>>
+    {
+        private readonly Dictionary<string, string> metadata = new Dictionary<string, string>();
+
+        public FakeTaskItem(string itemSpec)
+        {
+            this.ItemSpec = itemSpec;
+        }
+
+        // For object initializer
+        public void Add(string metadataName, string metadataValue)
+        {
+            SetMetadata(metadataName, metadataValue);
+        }
+
+        public string ItemSpec { get; set; }
+
+        public ICollection MetadataNames => metadata.Keys;
+
+        public int MetadataCount => metadata.Count;
+
+        public IDictionary CloneCustomMetadata()
+        {
+            return new Dictionary<string, string>(metadata);
+        }
+
+        public void CopyMetadataTo(ITaskItem destinationItem)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string GetMetadata(string metadataName)
+        {
+            if (metadata.TryGetValue(metadataName, out string value))
+            {
+                return value;
+            }
+
+            return string.Empty;
+        }
+
+        public void RemoveMetadata(string metadataName)
+        {
+            metadata.Remove(metadataName);
+        }
+
+        public void SetMetadata(string metadataName, string metadataValue)
+        {
+            metadata[metadataName] = metadataValue;
+        }
+
+        public IEnumerator<KeyValuePair<string, string>> GetEnumerator()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return metadata.GetEnumerator();
+        }
+    }
+
+}
