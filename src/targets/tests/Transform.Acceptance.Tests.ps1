f7e130e454bef3da85696708a3399a75b72f1a03
Richard Szalay <richard@richardszalay.com>
2018-05-13 15:15:13 +1000

    Add support for including the merged transform with the package
    Allows transform to be applied to external Web.config at deploy-time

---
+++ src/targets/tests/Transform.Acceptance.Tests.ps1
@@ -0,0 +1,76 @@
+. "$PSScriptRoot/utils/WebConfig.ps1"
+. "$PSScriptRoot/utils/MSBuild.ps1"
+. "$PSScriptRoot/utils/MSDeploy.ps1"
+
+$fixtures = @{
+    default = @{
+        Solution = "$PSScriptRoot\fixtures/default/HelixBuild.Sample.Web.sln";
+        Project1 = "$PSScriptRoot\fixtures/default/Projects\HelixBuild.Sample.Web\code\HelixBuild.Sample.Web.csproj";
+    }
+    
+}
+
+$count = 1
+
+Describe "Module Web.config transforms" {
+<#
+    Context "building package with default settings" {
+        $projectPath = $fixtures.default.Project1
+        $projectDir = Split-Path $projectPath -Parent
+        
+        Invoke-MSBuild -Project $projectPath -Properties @{
+            "HelixTargetsConfiguration" = "WebRoot"; # This is only supported by the test fixture
+            "Configuration" = "Debug";
+            "DeployOnBuild" = "true";
+            "PublishProfile" = "Package";
+            "DeployAsIisApp" = "false";
+        }
+
+        $packageFilename = Join-Path $projectDir "obj\Debug\Package\HelixBuild.Sample.Web.zip"
+
+        $packageParameters = Get-MSDeployPackageParameters $packageFilename
+        $packageParameterNames = $packageParameters.Keys
+
+        $packageFiles = Get-MSDeployPackageFiles $packageFilename
+
+        $webConfigXml = [xml](Get-MSDeployPackageFileContent -PackagePath $packageFilename -FilePath "Web.config")
+
+        It "should include standard Web.config transforms from the packaged project" {
+            (Get-WebConfigAppSetting $webConfigXml "Project.ConfigKey") | Should Be "Project.ConfigValue"
+        }
+
+        It "should include Web.Helix.config transforms from feature modules" {
+            (Get-WebConfigAppSetting $webConfigXml "Feature1.ConfigKey") | Should Be "Feature1.ConfigValue"
+        }
+
+        It "should not include Web.Helix.config transforms from indirect module dependencies" {
+            (Get-WebConfigAppSetting $webConfigXml "Foundation1.ConfigKey") | Should Be $null
+        }
+
+        It "should not include merged Web.Helix.config transform in published output" {
+            $packageFiles -contains "Web.Helix.config" | Should Be $false
+        }
+    }
+#>
+    Context "building package with IncludeHelixWebConfigTransformInPackage enabled" {
+        $projectPath = $fixtures.default.Project1
+        $projectDir = Split-Path $projectPath -Parent
+        
+        Invoke-MSBuild -Project $projectPath -Properties @{
+            "HelixTargetsConfiguration" = "Module"; # This is only supported by the test fixture
+            "Configuration" = "Debug";
+            "DeployOnBuild" = "true";
+            "PublishProfile" = "Package";
+            "DeployAsIisApp" = "false";
+            "IncludeHelixWebConfigTransformInPackage" = "true";
+        }
+
+        $packageFilename = Join-Path $projectDir "obj\Debug\Package\HelixBuild.Sample.Web.zip"
+
+        $packageFiles = Get-MSDeployPackageFiles $packageFilename
+
+        It "should include merged Web.Helix.config transform in published output" {
+            $packageFiles -contains "Web.Helix.config" | Should Be $true
+        }
+    }
+}
