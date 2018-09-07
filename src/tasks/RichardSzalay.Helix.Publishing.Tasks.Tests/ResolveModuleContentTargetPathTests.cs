f8e1be6fd4aa1c7914d98bf70c31348626bba19e
Richard Szalay <richard@richardszalay.com>
2018-09-07 21:26:33 +1000

    Add AdditionalFilesForPackagingFromHelixModules syntax for advanced scenarios (#18)

---
+++ src/tasks/RichardSzalay.Helix.Publishing.Tasks.Tests/ResolveModuleContentTargetPathTests.cs
@@ -0,0 +1,240 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace RichardSzalay.Helix.Publishing.Tasks.Tests
+{
+    public class ResolveModuleContentTargetPathTests
+    {
+        private readonly ResolveModuleContentTargetPath sut;
+
+        public ResolveModuleContentTargetPathTests()
+        {
+            sut = new ResolveModuleContentTargetPath()
+            {
+                ModuleMetadataPrefix = "HelixModule",
+                TargetPathMetadataName = "TargetPath",
+                BuildEngine = new FakeBuildEngine()
+            };
+        }
+
+        [Fact]
+        public void Uses_relative_path_when_no_TargetPath_specified()
+        {
+            Test(
+                CreateModuleItem(@"c:\path\to\TestModule.csproj"),
+                new[]
+                {
+                    CreateContentItem(@"c:\path\to\relative\content1.txt"),
+                    CreateContentItem(@"c:\path\to\relative\content2.txt")
+                },
+                success: true,
+                expectedTargetPaths: new[]
+                {
+                    @"relative\content1.txt",
+                    @"relative\content2.txt"
+                }
+            );
+        }
+
+        [Fact]
+        public void Throws_when_no_TargetPath_specified_but_paths_cannot_be_made_relative()
+        {
+            Test(
+                CreateModuleItem(@"c:\path\to\TestModule.csproj"),
+                new[]
+                {
+                    CreateContentItem(@"d:\path\to\relative\content1.txt")
+                },
+                success: false,
+                expectedTargetPaths: new string[0]
+            );
+        }
+
+        [Fact]
+        public void Expands_templated_TargetPath_when_specified()
+        {
+            Test(
+                CreateModuleItem(@"c:\path\to\TestModule.csproj"),
+                new[]
+                {
+                    CreateContentItem(
+                        @"d:\path\to\relative\content1.txt",
+                        new Dictionary<string, string>
+                        {
+                            ["TargetPath"] = @"^(Var1)\^(Var2)",
+                            ["Var1"] = @"Value1",
+                            ["Var2"] = @"Value2"
+                        }
+                    )
+                },
+                success: true,
+                expectedTargetPaths: new []
+                {
+                    @"Value1\Value2"
+                }
+            );
+        }
+
+        [Fact]
+        public void Templated_TargetPath_prefers_content_metadata()
+        {
+            Test(
+                CreateModuleItem(
+                    @"c:\path\to\TestModule.csproj",
+                    new Dictionary<string, string>
+                    {
+                        ["TargetPath"] = @"^(Var1)\^(Var2)",
+                        ["Var1"] = @"Module1",
+                        ["Var2"] = @"Module2"
+                    }
+                ),
+                new[]
+                {
+                    CreateContentItem(
+                        @"d:\path\to\relative\content1.txt",
+                        new Dictionary<string, string>
+                        {
+                            ["TargetPath"] = @"^(Var1)\^(Var2)",
+                            ["Var1"] = @"Value1",
+                            ["Var2"] = @"Value2"
+                        }
+                    )
+                },
+                success: true,
+                expectedTargetPaths: new[]
+                {
+                    @"Value1\Value2"
+                }
+            );
+        }
+        
+        [Fact]
+        public void Templated_TargetPath_uses_module_metadata_when_requested()
+        {
+            Test(
+                CreateModuleItem(
+                    @"c:\path\to\TestModule.csproj",
+                    new Dictionary<string, string>
+                    {
+                        ["Var1"] = @"Module1",
+                        ["Var2"] = @"Module2"
+                    }
+                ),
+                new[]
+                {
+                    CreateContentItem(
+                        @"d:\path\to\relative\content1.txt",
+                        new Dictionary<string, string>
+                        {
+                            ["TargetPath"] = @"^(HelixModule.Filename)\^(HelixModule.Var2)",
+                            ["Var1"] = @"Value1",
+                            ["Var2"] = @"Value2"
+                        }
+                    )
+                },
+                success: true,
+                expectedTargetPaths: new[]
+                {
+                    @"TestModule\Module2"
+                }
+            );
+        }
+
+        [Fact]
+        public void Fails_when_templated_TargetPath_is_not_relative()
+        {
+            Test(
+                CreateModuleItem(@"c:\path\to\TestModule.csproj"),
+                new[]
+                {
+                    CreateContentItem(
+                        @"d:\path\to\relative\content1.txt",
+                        new Dictionary<string, string>
+                        {
+                            ["TargetPath"] = @"..\another\path",
+                            ["Var1"] = @"Value1",
+                            ["Var2"] = @"Value2"
+                        }
+                    )
+                },
+                success: false,
+                expectedTargetPaths: new string[0]
+            );
+        }
+
+        [Fact]
+        public void Fails_when_templated_TargetPath_is_rooted()
+        {
+            Test(
+                CreateModuleItem(@"c:\path\to\TestModule.csproj"),
+                new[]
+                {
+                    CreateContentItem(
+                        @"d:\path\to\relative\content1.txt",
+                        new Dictionary<string, string>
+                        {
+                            ["TargetPath"] = @"c:\another\path",
+                            ["Var1"] = @"Value1",
+                            ["Var2"] = @"Value2"
+                        }
+                    )
+                },
+                success: false,
+                expectedTargetPaths: new string[0]
+            );
+        }
+
+        private void Test(ITaskItem moduleItem, ITaskItem[] contentItems, bool success, string[] expectedTargetPaths)
+        {
+            sut.Module = moduleItem;
+            sut.Content = contentItems;
+            var result = sut.Execute();
+
+            Assert.Equal(success, result);
+
+            if (success)
+            {
+                Assert.Equal(
+                    expectedTargetPaths,
+                    sut.Output.Select(i => i.GetMetadata("TargetPath"))
+                );
+            }
+        }
+
+        private ITaskItem CreateContentItem(string path, IDictionary<string, string> metadata = null)
+        {
+            var item = new TaskItem(path);
+
+            if (metadata != null)
+            {
+                foreach (var kvp in metadata)
+                {
+                    item.SetMetadata(kvp.Key, kvp.Value);
+                }
+            }
+
+            return item;
+        }
+
+        private ITaskItem CreateModuleItem(string path, IDictionary<string, string> metadata = null)
+        {
+            var item = new TaskItem(path);
+
+            if (metadata != null)
+            {
+                foreach (var kvp in metadata)
+                {
+                    item.SetMetadata(kvp.Key, kvp.Value);
+                }
+            }
+
+            return item;
+        }
+    }
+}
---
+++ src/tasks/RichardSzalay.Helix.Publishing.Tasks/ResolveModuleContentTargetPath.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace RichardSzalay.Helix.Publishing.Tasks
+{
+    public class ResolveModuleContentTargetPath : Task
+    {
+        [Required]
+        public ITaskItem Module { get; set; }
+
+        [Required]
+        public ITaskItem[] Content { get; set; }
+
+        [Required]
+        public string TargetPathMetadataName { get; set; }
+
+        [Required]
+        public string ModuleMetadataPrefix { get; set; }
+
+        [Output]
+        public ITaskItem[] Output { get; set; }
+
+        public override bool Execute()
+        {
+            var output = new List<ITaskItem>();
+
+            foreach (var content in Content)
+            {
+                var resolvedTargetPath = ResolveTargetPath(content, Module);
+
+                if (Path.IsPathRooted(resolvedTargetPath) || resolvedTargetPath.StartsWith(".."))
+                {
+                    var contentFullPath = content.GetMetadata("FullPath");
+                    var targetPath = content.GetMetadata(TargetPathMetadataName);
+
+                    if (string.IsNullOrEmpty(targetPath))
+                    {
+                        Log.LogError($"Could not resolve {TargetPathMetadataName} for {contentFullPath}");
+                    }
+                    else
+                    {
+                        Log.LogError($"Could not resolve {TargetPathMetadataName} '{targetPath}' to a relative path for {contentFullPath}");
+                    }
+
+                    return false;
+                }
+                // TODO: Write error if resolvedTargetPath is absolute or starts with ..
+
+                var cloned = new TaskItem(content);
+
+                cloned.SetMetadata(TargetPathMetadataName, resolvedTargetPath);
+
+                output.Add(cloned);
+            }
+
+            Output = output.ToArray();
+
+            return true;
+        }
+
+        private string ResolveTargetPath(ITaskItem content, ITaskItem module)
+        {
+            var targetPathTemplate = content.GetMetadata(TargetPathMetadataName);
+
+            if (string.IsNullOrEmpty(targetPathTemplate))
+            {
+                return GetRelativePath(content, module);
+            }
+
+            return Regex.Replace(targetPathTemplate, @"\^\(([^)]+)\)", (match) =>
+            {
+                var key = match.Groups[1].Value;
+
+                if (key.StartsWith($"{ModuleMetadataPrefix}."))
+                {
+                    var moduleKey = key.Substring(ModuleMetadataPrefix.Length + 1);
+
+                    return module.GetMetadata(moduleKey);
+                }
+
+                if (content.MetadataNames.OfType<string>().Contains(key))
+                {
+                    return content.GetMetadata(key);
+                }
+
+                return content.GetMetadata(key);
+            });
+        }
+
+        private static string GetRelativePath(ITaskItem content, ITaskItem relativeTo)
+        {
+            // TODO: Throw if result is absolute
+            return MakeRelativePath(
+                relativeTo.GetMetadata("FullPath"),
+                content.GetMetadata("FullPath")                
+            );
+        }
+
+        private static string MakeRelativePath(string fromPath, string toPath)
+        {
+            if (string.IsNullOrEmpty(fromPath)) throw new ArgumentNullException("fromPath");
+            if (string.IsNullOrEmpty(toPath)) throw new ArgumentNullException("toPath");
+
+            Uri fromUri = new Uri(fromPath);
+            Uri toUri = new Uri(toPath);
+
+            if (fromUri.Scheme != toUri.Scheme) { return toPath; } // path can't be made relative.
+
+            Uri relativeUri = fromUri.MakeRelativeUri(toUri);
+            string relativePath = Uri.UnescapeDataString(relativeUri.ToString());
+
+            if (toUri.Scheme.Equals("file", StringComparison.InvariantCultureIgnoreCase))
+            {
+                relativePath = relativePath.Replace(Path.AltDirectorySeparatorChar, Path.DirectorySeparatorChar);
+            }
+
+            return relativePath;
+        }
+    }
+}
