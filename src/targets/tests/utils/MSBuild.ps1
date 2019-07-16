6d3ad0f8cfcb0ffefceb46192955b9549376fc25
Richard Szalay <richard@richardszalay.com>
2019-07-16 13:51:23 +1000

    Fix deadlock scenario in build

--- src/targets/tests/utils/MSBuild.ps1
+++ src/targets/tests/utils/MSBuild.ps1
@@ -119,15 +119,15 @@ function Invoke-MSBuild
     $pinfo = New-Object System.Diagnostics.ProcessStartInfo
     $pinfo.FileName = $msBuildExe
     $pinfo.RedirectStandardError = $true
-    #$pinfo.RedirectStandardOutput = $true
+    $pinfo.RedirectStandardOutput = $true
     $pinfo.UseShellExecute = $false
     $pinfo.Arguments = $arguments
     $p = New-Object System.Diagnostics.Process
     $p.StartInfo = $pinfo
     $p.Start() | Out-Null
-    $p.WaitForExit()
-    #$stdout = $p.StandardOutput.ReadToEnd()
+    $stdout = $p.StandardOutput.ReadToEnd()
     $stderr = $p.StandardError.ReadToEnd()
+    $p.WaitForExit()
 
     if ($p.ExitCode -ne 0) {
         throw $stderr
